dc37ab2c4e6295703e2f121fd69285879ed49365
mark.haywood <mark.haywood@sortrefer.co.uk>
2025-04-16 19:53:22 +0100

    Initial load.

---
+++ src/components/Ed209.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="bg-white">
+      <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 class="sr-only">Products</h2>
+  
+        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <a v-for="product in products" :key="product.id" :href="product.href" class="group">
+            <img :src="product.imageSrc" :alt="product.imageAlt" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8" />
+            <h3 class="mt-4 text-sm text-gray-700">{{ product.name }}</h3>
+            <p class="mt-1 text-lg font-medium text-gray-900">{{ product.price }}</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const products = [
+    {
+      id: 1,
+      name: 'How to build you own Enforcement Droid Series 209',
+      href: '#',
+      price: '£0',
+      imageSrc: 'img/ED-209.jpg',
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
+    {
+      id: 2,
+      name: 'Sketch your ED-209',
+      href: '#',
+      price: '£0',
+      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-02.jpg',
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+      id: 3,
+      name: 'Blender your ED-209',
+      href: '#',
+      price: '£0',
+      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-03.jpg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 4,
+      name: 'Build your ED-209',
+      href: '#',
+      price: '£0',
+      imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-04.jpg',
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    // More products...
+  ]
+  </script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
+
+
